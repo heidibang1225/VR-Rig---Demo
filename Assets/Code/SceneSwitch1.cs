6392b574bce55d0bdd0fd7a718c74a5fb3f77a9a
Emily Xie <Xiee178@newschool.edu>
2021-11-07 01:52:42 -0400

    trying to make levels in scene work
    still need to work on the code

---
+++ Assets/Code/SceneSwitch1.cs
@@ -0,0 +1,25 @@
+//this will automatically load my next scene
+//atach this script to the object the player will enter to collide
+
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitch1 : MonoBehaviour
+{
+
+
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+
+        }
+    }
+
+
+}
