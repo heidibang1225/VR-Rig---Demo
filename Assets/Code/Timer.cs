e2b35cebf3507863bda54fb307744bf6f2833a52
Emily Xie <Xiee178@newschool.edu>
2021-11-10 20:47:47 -0500

    trying to figure out timer

--- Assets/Code/Timer.cs
+++ Assets/Code/Timer.cs
@@ -1,18 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Timer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float timeValue = 90;
+    public Text timerText;
+    bool TimerStarted = false;
+   
+    void OnCollisionEnter(Collision other)
     {
-        
+        if (other.gameObject.CompareTag("Player") && !TimerStarted)
+        {
+            StartCoroutine(TimerStarted());
+            TimerStarted = true;
+        }
     }
-
-    // Update is called once per frame
-    void Update()
+    IEnumerator TimerStarted()
     {
+        float TimeToDisplay = 0;
+        while (true)//this is a loop to check if it is true
+        {
+           
+            float minutes = Mathf.FloorToInt(timeToDisplay / 60);
+            float seconds = Mathf.FloorToInt(timeToDisplay % 60);//modulo operation that will returns the remainder, after dividison of one number by another
+                                                                 //in this example, modulo is used to return the number of seconds from the total time value that do not make up a whole minute
+            timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+            TimeToDisplay += 1;
+            yeild return new WaitForSeconds(1);
+        }
         
     }
 }
