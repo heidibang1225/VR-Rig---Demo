fad7fabe20d4eee301da524e07355b55079c1d83
Emily Xie <Xiee178@newschool.edu>
2021-11-12 19:10:01 -0500

    trying to figure out cut scenes now

--- Assets/Code/Timer.cs
+++ Assets/Code/Timer.cs
@@ -24,7 +24,17 @@ public class Timer : MonoBehaviour
         //while Loops can execute a block of code as long as a specified condition is reached.
         while (true)//this is a loop to check if it is true
         {
-           
+            if (TimeToDisplay > 0)//TimeToDisplay is at 90 right now if it's bigger than zero, then start -1
+            {
+                TimeToDisplay -= 1;
+            }
+            else if (TimeToDisplay == 0)//if TimeToDisplay turns into zero, then blablabla happens
+            {
+                Debug.Log("yessssssss time ran out");
+                //TimeToDisplay -= 1;
+            }
+
+
             //to display the time correctly, both minutes and seconds need to be calculted individually from the raw time value 
             float minutes = Mathf.FloorToInt(TimeToDisplay / 60);
             float seconds = Mathf.FloorToInt(TimeToDisplay % 60);//modulo operation that will returns the remainder, after dividison of one number by another
@@ -33,13 +43,12 @@ public class Timer : MonoBehaviour
             //{0:00} formating option with zerops as placeholders       this is variable "1" seconds
             //{1:00} this is variable "2" minutes
             //TimeToDisplay += 1;//this is the same thing to TimeToDisplay++
-            TimeToDisplay -= 1;
+           // TimeToDisplay -= 1;
             yield return new WaitForSeconds(1);
-            
 
+            //break;
         }
-        
-        
+      
     }
 }
  
